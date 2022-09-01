b61983169f882e95f970bbbaf8199f8c2eb7d42f
Viktorhs <victor.hugo.gba@hotmail.com.br>
2022-09-01 16:44:51 -0300

    adicionado o Post das mensagens

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.post("/participants", async (req, res) => {
     try {
         const participants = await db.collection('participants').findOne({name});
         if(!!participants){
-            return res.sendStatus(409)
+            return res.sendStatus(409);
         }
 
         await db.collection('participants').insertOne({name, lastStatus: Date.now()});
@@ -36,26 +36,66 @@ app.post("/participants", async (req, res) => {
             text: 'entra na sala...', 
             type: 'status', 
             time: dayjs().format("HH:mm:ss")
-        }
-        await db.collection('messages').insertOne(userLogin)
-        res.sendStatus(201)
+        };
+        await db.collection('messages').insertOne(userLogin);
+        res.sendStatus(201);
     } catch (error) {
-        res.sendStatus(500)
+        res.sendStatus(500);
     }
 })
 
 app.get("/participants", async (req, res) => {
 
     try {
-        const participants = await db.collection("participants").find({}).toArray()
-        res.send(participants)
+        const participants = await db.collection("participants").find({}).toArray();
+        res.send(participants);
     } catch (error) {
-        res.sendStatus(500)
+        res.sendStatus(500);
     }
-})
 
+});
 
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body;
+    const { user } = req.headers;
+
+    if(!to || !text || !type || !user){
+        return res.sendStatus(400)
+    }
+
+    try {
+        const participant = await db.collection("participants").findOne({name: user});
+        if(to.length === 0 || text.length === 0 || 
+            (type !== "message" && type !== "private_message") || !participant){
+            return res.sendStatus(422);
+        };
+
+        const message = {
+            from: user, 
+            to: to, 
+            text: text, 
+            type: type, 
+            time: dayjs().format("HH:mm:ss")
+        };
+
+        await db.collection('messages').insertOne(message);
+        res.sendStatus(201);
+
+    } catch (error) {
+        res,sendStatus(500);
+    }
+    
+});
+
+app.get("/messages", async (req, res) => {
+    try {
+        const messages = await db.collection("messages").find({}).toArray();
+        res.send(messages);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+});
 
 app.listen(5000, () => {
-    console.log('Listen on port 5000')
-})
+    console.log('Listen on port 5000');
+});
